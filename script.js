7e157a645962b0f603d45c6ea3ca180aedb7f954
siv12345 <babu.sivasurya@gmail.com>
2019-10-16 12:11:19 +0530

    Added Dark Mode, still making sidebar alignment

---
+++ script.js
@@ -0,0 +1,19 @@
+        var checkbox = document.querySelector('input[name=theme]');
+
+        checkbox.addEventListener('change', function() {
+            if(this.checked) {
+                trans()
+                document.documentElement.setAttribute('data-theme', 'light')
+            } else {
+                trans()
+                document.documentElement.setAttribute('data-theme', 'dark')
+            }
+        })
+
+        let trans = () => {
+            document.documentElement.classList.add('transition');
+            window.setTimeout(() => {
+                document.documentElement.classList.remove('transition')
+            }, 1000)
+        }
+
